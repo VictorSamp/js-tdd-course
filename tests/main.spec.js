acdb0955eaf594967291adca57307e07a47f498e
VictorSamp <victor.msampaio@outlook.com.br>
2021-02-19 16:56:28 -0300

    Finalizando desafio FizzBuzz implementando TDD

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,74 +1,25 @@
 import { expect } from 'chai';
-import {sum, sub, mult, div } from '../src/main';
+import FizzBuzz from '../src/main';
 
-describe('Calc', () => {
+describe('Main', () => {
 
-  // smoke tests
-  describe('Smoke tests', () => {
-
-    it('should exist the method `sum`', () =>  {
-      expect(sum).to.exist;
-    });
-
-    it('should exist the method `sub`', () =>  {
-      expect(sub).to.exist;
-    });
-
-    it('should exist the method `mult`', () =>  {
-      expect(mult).to.exist;
-    });
-
-    it('should exist the method `div`', () =>  {
-      expect(div).to.exist;
-    });
-
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
   });
 
-  describe('Sum', () => {
-
-    it('should return 4 when `sum(2,2)`', () => {
-      expect(sum(2, 2)).to.be.equal(4);
-    });
-
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
   });
 
-  describe('Sub', () => {
-
-    it('should return 4 when `sub(6,2)`', () => {
-      expect(sub(6, 2)).to.be.equal(4);
-    });
-
-    it('should return -4 when `sub(6,10)`', () => {
-      expect(sub(6, 10)).to.be.equal(-4);
-    });
-
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
   });
 
-  describe('Mult', () => {
-
-    it('should return 4 when `mult(2,2)`', () => {
-      expect(mult(2, 2)).to.be.equal(4);
-    });
-
-    it('should return -4 when `mult(-2,2)`', () => {
-      expect(mult(-2, 2)).to.be.equal(-4);
-    });
-
+  it('should return the number when non-multiple', () => {
+    expect(FizzBuzz(7)).to.be.equal(7);
   });
 
-  describe('Div', function() {
-
-    it('should return 4 when `div(2,2)`', function(){
-      expect(div(2, 2)).to.be.equal(1);
-    });
-
-    it('should return -4 when `div(-2,2)`', function(){
-      expect(div(-2, 2)).to.be.equal(-1);
-    });
-
-    it('should return `não é possível divisão por zero` when divide by 0', function() {
-      expect(div(4,0)).to.be.equal('não é possível divisão por zero');
-    })
-
-  });
 });
