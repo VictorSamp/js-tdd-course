465ad57cf7f42a8917360cfcb40d077ec61b075e
VictorSamp <victor.msampaio@outlook.com.br>
2021-02-23 13:32:16 -0300

    Utilizando CalledWith

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,12 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
+
 import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
 describe('Spotify Wrapper', () => {
@@ -28,7 +36,23 @@ describe('Spotify Wrapper', () => {
   describe('Generic Search', () => {
 
     it('should call fetch function', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
       const artists = search();
+
+      expect(fetchedStub).to.have.been.calledOnce;
+
+      fetchedStub.restore();
     });
+
+    it('should receive the correct url to fetch', () => {
+      const fetchedStub = sinon.stub(global, 'fetch');
+      const artist = search('Incubus', 'artist');
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+      const albums = search('Incubus', 'album');
+      expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album')
+    })
   });
 });
